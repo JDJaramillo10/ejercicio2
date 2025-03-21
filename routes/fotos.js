762f676dd72d7627558d6fd0ee74b96df19f6a16
JDJaramillo10 <juan.jaramillo@uees.edu.ec>
2025-03-20 23:10:23 -0500

    Entrega final CRUD

--- routes/fotos.js
+++ routes/fotos.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 const {Sequelize, Op} = require('sequelize');
-const etiqueta = require('../models/etiqueta');
 const Foto = require('../models').foto;
 const Etiqueta = require('../models').etiqueta;
 
